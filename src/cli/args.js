fa1d646fbf71da8684ae12433a4b5ce6087dc116
Maksim Malashkov <max.malaschkov@mail.ru>
2024-01-25 20:53:36 +0300

    feat(*): added node.js basics taska

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  let args = process.argv;
+  
+  args.forEach((item, index) => {
+    if (item.includes("--")) {
+      console.log(`${item} is ${args[index + 1]}`);
+    }
+  });
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,14 @@
 const parseEnv = () => {
-    // Write your code here 
+  process.env.RSS_name1 = "This should be displayed";
+  process.env.RSS_name2 = "As well as that";
+
+  let localEnv = process.env;
+  
+  for (let key in localEnv) {
+    if (key.includes("RSS_")) {
+      console.log(`${key} = ${localEnv[key]}`);
+    }
+  }
 };
 
 parseEnv();
